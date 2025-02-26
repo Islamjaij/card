3265819085e10c04d00b6a88ae7bd547c26dcbf9
Islamjaij <ana.haker.new@gmail.com>
2025-02-26 21:19:17 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,8 @@
-function updateTime() {
-    const now = new Date();
-    
-    // تحديث التاريخ
-    const date = now.toISOString().split('T')[0];
-    document.getElementById("current-date").textContent = date;
-
-    // تحديث الوقت
-    const time = now.toLocaleTimeString("ar-EG", { hour12: false });
-    document.getElementById("current-time").textContent = time;
+function updateDateTime() {
+    let now = new Date();
+    document.getElementById('date').innerText = now.toISOString().split('T')[0];
+    document.getElementById('time').innerText = now.toLocaleTimeString();
 }
 
-// تحديث الوقت كل ثانية
-setInterval(updateTime, 1000);
-
-// تشغيل الدالة عند تحميل الصفحة
-updateTime();
+setInterval(updateDateTime, 1000);
+updateDateTime();
